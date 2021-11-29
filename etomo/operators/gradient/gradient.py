d28742ca8f5d5ea8a954ded9df2779f4bd1736ee
Guillaume Biagi <biagiguillaume@gmail.com>
2021-11-29 10:16:02 +0100

    Added comments

--- etomo/operators/gradient/gradient.py
+++ etomo/operators/gradient/gradient.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-##########################################################################
-# pySAP - Copyright (C) CEA, 2017 - 2018
-# Distributed under the terms of the CeCILL-B license, as published by
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
-# for details.
-##########################################################################
 """
-This module contains classes for defining gradients operators.
+This module contains classes for defining gradients operators. Taken from
+pysap-mri with minor changes (name of parameters (data_op), change of data
+type to float).
 """
 import numpy as np
 
