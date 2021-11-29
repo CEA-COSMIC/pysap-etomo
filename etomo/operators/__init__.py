8f2d8c427cdab66b3f7df407a8696137705f1aec
Guillaume Biagi <biagiguillaume@gmail.com>
2021-11-29 10:23:54 +0100

    Removed pynfft

--- etomo/operators/__init__.py
+++ etomo/operators/__init__.py
@@ -1,16 +1,8 @@
-# -*- coding: utf-8 -*-
-##########################################################################
-# pySAP - Copyright (C) CEA, 2017 - 2018
-# Distributed under the terms of the CeCILL-B license, as published by
-# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
-# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
-# for details.
-##########################################################################
-
-""" This module defines the common operators.
+"""
+This module defines the common operators.
 """
 
 from .radon.radon import Radon2D, Radon3D
 from .gradient.gradient import GradAnalysis, GradSynthesis
 from .linear import HOTV, HOTV_3D, WaveletPywt
-from .fourier.fourier import NFFT, NUFFT2, gpuNUFFT
+from .fourier.fourier import NUFFT2, gpuNUFFT
