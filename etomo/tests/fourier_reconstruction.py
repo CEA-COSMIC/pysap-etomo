de8449fba0a84686e08544a51c1995cfc9a96e0d
Guillaume Biagi <biagiguillaume@gmail.com>
2021-11-29 10:26:15 +0100

    Updated example

--- etomo/tests/fourier_reconstruction.py
+++ etomo/tests/fourier_reconstruction.py
@@ -1,5 +1,6 @@
 """
-Example of a single image reconstruction
+Example of a single image reconstruction with fourier operators. See
+density_compensation for similar reconstruction with better results.
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +12,7 @@ from modopt.opt.linear import Identity
 from modopt.opt.proximity import SparseThreshold
 
 from etomo.operators.utils import generate_locations_etomo_2D
-from etomo.operators import NFFT, WaveletPywt, HOTV
+from etomo.operators import NUFFT2, WaveletPywt, HOTV
 from etomo.reconstructors.forwardtomo import TomoReconstructor
 from etomo.operators.fourier.utils import estimate_density_compensation
 
@@ -23,14 +24,12 @@ img_size = image.shape[0]
 # Create radon operator and simulate data
 theta = np.arange(0., 180., 3.)
 kspace_loc = generate_locations_etomo_2D(img_size, theta)
-fourier_op = NFFT(samples=kspace_loc, shape=image.shape, normalized=True)
+fourier_op = NUFFT2(samples=kspace_loc, shape=image.shape, normalized=True)
 data = fourier_op.op(image.data)
 
-TV = HOTV(img_shape=image.shape, order=1)
-wavelet = WaveletPywt(wavelet_name='sym8', nb_scale=3)
-linear_op = TV
+linear_op = WaveletPywt(wavelet_name='bior4.4', nb_scale=3)
 
-regularizer_op = SparseThreshold(linear=Identity(), weights=3e-6)
+regularizer_op = SparseThreshold(linear=Identity(), weights=1e-5)
 reconstructor = TomoReconstructor(
     data_op=fourier_op,
     linear_op=linear_op,
@@ -43,7 +42,7 @@ reconstructor = TomoReconstructor(
 x_final, cost, *_ = reconstructor.reconstruct(
     data=data,
     optimization_alg='condatvu',
-    num_iterations=200,
+    num_iterations=300,
     cost_op_kwargs={'cost_interval': 5}
 )
 
