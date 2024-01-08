7fd091744c7d78dbbf46e5810fbb81d026ce4084
Chaithya G R <chaithyagr@gmail.com>
2024-01-08 10:35:13 +0100

    minor version bump

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     classifiers="CLASSIFIERS",
     author=AUTHOR,
     author_email="XXX",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/CEA-COSMIC/pysap-etomo",
     packages=find_packages(),
     setup_requires=setup_requires,
